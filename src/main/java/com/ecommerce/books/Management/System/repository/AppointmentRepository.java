d1094529e6838dde9be69664cb20d0fca3ddeb35
Habeeb <habeeebkinlolu234@.com>
2024-10-17 18:57:35 +0100

    deleted all exixting files for a new project

--- src/main/java/com/ecommerce/books/Management/System/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.books.Management.System.repository;
-
-import com.ecommerce.books.Management.System.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment,Integer> {
-}
