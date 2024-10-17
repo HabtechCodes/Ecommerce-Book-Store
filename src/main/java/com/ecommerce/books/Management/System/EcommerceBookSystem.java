e3ccfbccd81eeadc41136317081842a4a023fc0e
Habeeb <habeeebkinlolu234@.com>
2024-10-17 17:46:13 +0100

    deleted some files

--- src/main/java/com/ecommerce/books/Management/System/EcommerceBookSystem.java
+++ src/main/java/com/ecommerce/books/Management/System/EcommerceBookSystem.java
@@ -1,13 +1,13 @@
-package com.hosptal.Healthcare.Management.System;
+package com.ecommerce.books.Management.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthcareManagementSystemApplication {
+public class EcommerceBookSystem {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthcareManagementSystemApplication.class, args);
+		SpringApplication.run(EcommerceBookSystem.class, args);
 	}
 
 }
